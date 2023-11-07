65e9f177fc64e2828003357dcae0d111f4e850cd
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-11-08 02:47:31 +0600

    successfully got data from mongodb server

--- index.js
+++ index.js
@@ -31,12 +31,18 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    
-
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const jobCollection = client.db('jobsDB').collection('jobs');
+
+    app.get('/jobs', async(req, res)=>{
+        const cursor = jobCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
