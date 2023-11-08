5cb023ba4104690ae99aeec9baca7aecf9686414
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-11-08 12:04:25 +0600

    single job data is fetched by get operattion

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -40,6 +40,13 @@ async function run() {
     app.get('/jobs', async(req, res)=>{
         const cursor = jobCollection.find();
         const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/job/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await jobCollection.findOne(query);
         res.send(result);
     })
 
